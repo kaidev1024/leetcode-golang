e9bd8cf403f59a77a4f1e7fbb2fd1d23b73ff4a8
kaidev1024 <58545754+kaidev1024@users.noreply.github.com>
2020-11-10 22:21:36 -0800

    add compare int slice

---
+++ utils/cmp/slicecmp.go
@@ -0,0 +1,18 @@
+package cmp
+
+func CmpIntSlice(arr1, arr2 []int) int8 {
+	l1 := len(arr1)
+	l2 := len(arr2)
+	if l1 > l2 {
+		return -CmpIntSlice(arr2, arr1)
+	}
+
+	for i, val := range arr1 {
+		if val < arr2[i] {
+			return -1
+		} else if val > arr2[i] {
+			return 1
+		}
+	}
+	return 0
+}
