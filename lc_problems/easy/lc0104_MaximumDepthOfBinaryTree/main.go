0fee989978a29cd2ebd955c74e0b216a4c338e6a
kaidev1024 <58545754+kaidev1024@users.noreply.github.com>
2020-11-12 23:18:46 -0800

    add lc104 max depth of binary tree

---
+++ lc_problems/easy/lc0104_MaximumDepthOfBinaryTree/main.go
@@ -0,0 +1,20 @@
+import (
+	"fmt"
+
+	"github.com/kaidev1024/leetcode-golang/structs/tree"
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
